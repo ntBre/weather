fad11e31bd3cbda743e6eb75449f895383c5fc6c
Brent Westbrook <brentrwestbrook@gmail.com>
2022-01-30 21:08:48 -0600

    add README and example figure; write code to generate that figure

--- weather.go
+++ weather.go
@@ -5,11 +5,16 @@ import (
 	"encoding/xml"
 	"fmt"
 	"net/http"
+	"os"
+	"os/exec"
 	"strings"
+	"time"
 )
 
 const (
-	url = "https://forecast.weather.gov/MapClick.php?lat=34.3651&lon=-89.5196&FcstType=digitalDWML"
+	url      = "https://forecast.weather.gov/MapClick.php?lat=34.3651&lon=-89.5196&FcstType=digitalDWML"
+	longFmt  = "2006-01-02T15:04:05-07:00"
+	shortFmt = "01-02/15:00"
 )
 
 type Result struct {
@@ -44,14 +49,14 @@ type Parameters struct {
 
 type Temperature struct {
 	// Type is either "dew point", "wind chill", or "hourly"
-	Type  string   `xml:"type,attr"`
-	Value []string `xml:"value"`
+	Type  string `xml:"type,attr"`
+	Value []int  `xml:"value"`
 }
 
 type Wind struct {
 	// Type is either "sustained" or "gust"
-	Type  string   `xml:"type,attr"`
-	Value []string `xml:"value"`
+	Type  string `xml:"type,attr"`
+	Value []int  `xml:"value"`
 }
 
 func main() {
@@ -66,22 +71,55 @@ func main() {
 			byts = append(byts, []byte(line+"\n")...)
 		}
 	}
-	// fmt.Printf("%s\n", byts)
 	res := new(Result)
 	err = xml.Unmarshal(byts, res)
 	if err != nil {
 		panic(err)
 	}
-	// fmt.Printf("%s\n", res.Data.Parameters.Temps[0])
+	temps := make(map[string][]int)
 	for _, t := range res.Data.Parameters.Temps {
-		fmt.Println(t.Type)
+		temps[t.Type] = t.Value
 	}
+	winds := make(map[string][]int)
 	for _, t := range res.Data.Parameters.Winds {
-		fmt.Println(t.Type)
+		winds[t.Type] = t.Value
 	}
-	fmt.Println(len(res.Data.Parameters.Clouds))
-	fmt.Println(len(res.Data.Parameters.Rain))
-	fmt.Println(len(res.Data.Times.Values))
-	fmt.Println(len(res.Data.Parameters.Humidity))
-	fmt.Println(len(res.Data.Parameters.Direction))
+	// fmt.Println(len(res.Data.Parameters.Clouds))
+	// fmt.Println(len(res.Data.Parameters.Rain))
+	// fmt.Println(len(res.Data.Times.Values))
+	// fmt.Println(len(res.Data.Parameters.Humidity))
+	// fmt.Println(len(res.Data.Parameters.Direction))
+	now := time.Now().Format(longFmt)
+	var start int
+	// make sure we start at the current time
+	for i, t := range res.Data.Times.Values {
+		if t == now {
+			start = i
+			break
+		}
+	}
+	end := start + 24
+	if l := len(res.Data.Times.Values); l < end {
+		end = l
+	}
+	f, err := os.Create("/tmp/weather.dat")
+	if err != nil {
+		panic(err)
+	}
+	for i, t := range res.Data.Times.Values[start : end+1] {
+		tt, _ := time.Parse(longFmt, t)
+		fmt.Fprintf(f, "%s %d\n", tt.Format(shortFmt), temps["hourly"][i])
+	}
+	cmd := exec.Command("gnuplot", "--persist")
+	// set xrange ["11/24/21":"12/01/21"]
+	cmd.Stdin = strings.NewReader(`set terminal png medium size 640,480 font arial 12
+set output 'out.png'
+set bmargin 2.5
+unset key
+set xdata time
+set timefmt "%m-%d/%H:%M"
+set ylabel "Temperature (°F)"
+plot "/tmp/weather.dat" using 1:2 with linespoints
+`)
+	cmd.Run()
 }
